add403686d2c97b4ffcca4a5cd408bd88296c36f
merciamartins <merciaessavagaesua@gmail.com>
2025-09-15 11:10:07 -0300

    Exercício 4: Declare duas variáveis chamadas nome e sobrenome, atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada nomeCompleto e imprima.

---
+++ exercicio04.js
@@ -0,0 +1,7 @@
+// Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+
+let nome = "Mércia";
+let sobrenome = "da Silva Soares Martins";
+let nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto)
