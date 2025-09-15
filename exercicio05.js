f2f1a12716e6e92f02ecb08194afb4ee7856bb71
merciamartins <merciaessavagaesua@gmail.com>
2025-09-15 11:11:12 -0300

    Exercício 5: Declare duas variáveis e atribua valores numéricos a elas. Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.

---
+++ exercicio05.js
@@ -0,0 +1,6 @@
+// Declare duas variáveis e atribua valores numéricos a elas. Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.
+
+let num1 = 10
+let num2 = 20
+
+console.log(num1 + num2)
