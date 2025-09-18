c659f44c2556e3ddbef70fc167ece7ee33879412
MerciaM <merciaessavagaesua@gmail.com>
2025-09-17 22:27:30 -0300

    Exercício 7: Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.

---
+++ exercicio07.js
@@ -0,0 +1,12 @@
+// Exercício 7: Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.
+
+let x = 10;
+let y = 20;
+let z = 10;
+
+console.log("x == y:", x == y);     
+console.log("x == z:", x == z);     
+console.log("x != y:", x != y);     
+console.log("y > x:", y > x);       
+console.log("x >= z:", x >= z);     
+console.log("z < y:", z < y);       
