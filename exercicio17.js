ee735ec2aac756ac86abd82c82d6ff5ca1cbf4bf
MerciaM <merciaessavagaesua@gmail.com>
2025-09-19 22:35:45 -0300

    Exercício 17: Escreva um programa que determina se um número é par ou ímpar.

---
+++ exercicio17.js
@@ -0,0 +1,9 @@
+// Exercício 17: Escreva um programa que determina se um número é par ou ímpar.
+
+let numero = parseInt(prompt("Digite um número:"));
+
+if (numero % 2 === 0) {
+    console.log(numero + " é par");
+} else {
+    console.log(numero + " é ímpar");
+}
