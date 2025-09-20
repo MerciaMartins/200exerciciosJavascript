0dc30d27337e5f5bafc21de6e1c7aa88f794e13b
MerciaM <merciaessavagaesua@gmail.com>
2025-09-19 22:38:54 -0300

    Exercício 18: Escreva um programa que verifica se uma pessoa pode dirigir com base na idade.

---
+++ exercicio18.js
@@ -0,0 +1,9 @@
+// Exercício 18: Escreva um programa que verifica se uma pessoa pode dirigir com base na idade.
+
+let idade = parseInt(prompt("Digite sua idade:"));
+
+if (idade >= 18) {
+    console.log("Você pode dirigir!");
+} else {
+    console.log("Você ainda não pode dirigir.");
+}
