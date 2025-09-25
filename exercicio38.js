58b09c08c7727750862a7c05c3928f6a5fb2cd06
MerciaM <merciaessavagaesua@gmail.com>
2025-09-24 21:45:59 -0300

    Exercício 38: Usando um laço do-while, imprima a soma dos cubos dos primeiros 10 números naturais.

--- exercicio38.js
+++ exercicio38.js
@@ -1,2 +1,12 @@
-// Exercício 38: enunciado não fornecido pelo professor.
-// Arquivo criado para manter a sequência.
+
+// Exercício 38: Usando um laço do-while, imprima a soma dos cubos dos primeiros 10 números naturais.
+
+let i = 1;
+let soma = 0;
+
+do {
+    soma += i ** 3; // ou Math.pow(i, 3)
+    i++;
+} while (i <= 10);
+
+console.log("A soma dos cubos dos primeiros 10 números naturais é:", soma);
