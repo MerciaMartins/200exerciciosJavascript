f7d66485965833cace9dadf465c1f91dc72a7d68
MerciaM <merciaessavagaesua@gmail.com>
2025-10-03 09:08:42 -0300

    Exercício 76: Crie um objeto vazio e adicione três propriedades a ele, cada uma com diferentes tipos de valores.

---
+++ exercicio76.js
@@ -0,0 +1,11 @@
+// Exercício 76: Crie um objeto vazio e adicione três propriedades a ele, cada uma com diferentes tipos de valores.
+
+
+var pessoa = {};
+
+
+pessoa.nome = "Ana";         
+pessoa.idade = 25;           
+pessoa.estaAtivo = true;    
+
+console.log(pessoa);
