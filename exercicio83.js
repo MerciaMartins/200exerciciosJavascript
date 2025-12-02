3613b6333cea46ff9607c724158044d937513730
MerciaM <merciaessavagaesua@gmail.com>
2025-12-02 04:58:15 -0300

    Exercício 83: Crie um objeto tempo que possui propriedades para horas, minutos e segundos, e um método para converter o tempo para segundos.

---
+++ exercicio83.js
@@ -0,0 +1,17 @@
+/* 
+
+Exercício 83: Crie um objeto "tempo" que possui propriedades para horas, minutos e segundos,
+e um método para converter o tempo para segundos.
+
+*/
+const tempo = {
+  horas: 2,
+  minutos: 20,
+  segundos: 45,
+  converterParaSegundos: function() {
+    return this.horas * 3600 + this.minutos * 60 + this.segundos;
+  }
+};
+
+
+console.log(`${tempo.horas} hora(s), ${tempo.minutos} minuto(s) e ${tempo.segundos} segundo(s) equivalem a um total de ${tempo.converterParaSegundos()} segundos`);
